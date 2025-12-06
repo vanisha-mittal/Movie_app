3670650f624aa947f7de5b9af23a4a0c4bb4df4d
vanisha-mittal <vanishamittal1409@gmail.com>
2025-12-06 18:28:21 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
