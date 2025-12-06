69aff4835ee79c96b81045db45c9f5432893d9e9
vanisha-mittal <vanishamittal1409@gmail.com>
2025-12-06 18:16:00 +0530

    connect frontend

--- backend/app.js
+++ backend/app.js
@@ -21,5 +21,19 @@ mongoose
 
 app.use("/api/search", searchRoutes);
 
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
